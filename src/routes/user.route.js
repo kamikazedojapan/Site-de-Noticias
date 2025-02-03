a0006c404f3d1120380141952a4128a4c267b5f8
kamikazedojapan <vi.nimarcio@hormail.com>
2025-02-03 00:07:56 -0300

    routes findAll and findById

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,9 @@
 const route = require('express').Router()
 const userController = require('../controllers/user.controllers')
 
-route.post('/', userController.create)
+route.post('/', userController.create) 
+route.get('/', userController.findAll) // 
+route.get('/:id', userController.findById) //
+
 
 module.exports = route;
