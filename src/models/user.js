090d463cf161f36a46673cfa7510d65b6dfff341
kamikazedojapan <vi.nimarcio@hormail.com>
2025-02-04 19:05:42 -0300

    encrypting the password

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -23,7 +24,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        select: false
+        // select: false
     },
     avatar: {
         type: String
@@ -34,6 +35,11 @@ const UserSchema = new mongoose.Schema({
 },
 
 { timestamps: true }); // Cria automaticamente `createdAt` e `updatedAt`
+
+UserSchema.pre("save", async function(next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 const User = mongoose.model("User", UserSchema);
 
