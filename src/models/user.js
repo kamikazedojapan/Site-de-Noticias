99bac219a15fde798f5dd4e237517929bfb4611e
kamikazedojapan <vi.nimarcio@hormail.com>
2025-02-04 19:12:20 -0300

    select: false

--- src/models/user.js
+++ src/models/user.js
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        // select: false
+        select: false
     },
     avatar: {
         type: String
