f8d0fd324691f487c9c308d5632a3f6dfb021306
kamikazedojapan <vi.nimarcio@hormail.com>
2025-02-12 15:04:58 -0300

    route search by title

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import {createService, findAllService, countNews, topNewsService, findByIdService} from "../services/news.service.js"
+import {createService, findAllService, countNews, topNewsService, findByIdService, searchByTitleService} from "../services/news.service.js"
 
 const create = async (req, res) => {
     try {
@@ -63,7 +63,7 @@ const findAll = async (req, res) => {
                 comments: item.comments,
                 name: item.user.name,
                 username: item.user.username,
-                userAvatar: item.user.avatar,
+                userAvatar: item.user.avatar
             })),
         });
     } catch (err) {
@@ -76,7 +76,7 @@ const topNews = async (req, res) => {
         const news = await topNewsService();
     
         if (!news) {
-            return res.status(400).send({message: "There is no registered post"});
+            res.status(400).send({message: "There is no registered post"});
         }
     
         res.send({
@@ -103,7 +103,7 @@ const findById = async (req, res) => {
         
         const news = await findByIdService(id)
         
-        res.send({
+        return res.send({
             news: {
                 id: news._id,
                 title: news.title,
@@ -120,4 +120,34 @@ const findById = async (req, res) => {
         res.status(500).send({ message: err.message });
     }
 }
-export { create, findAll, topNews , findById}
+
+const searchByTitle = async(req, res) => {
+    try {
+        const {title} = req.query;
+
+        const news = await searchByTitleService(title)
+
+        if(!news.length === 0) {
+            res.status(400).send({message: "There are no news with title"})
+        }
+        
+        res.send({
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar
+            })),
+        });
+    } catch (err) {
+        return res.status(500).send({ message: err.message });
+    }
+
+}
+
+export { create, findAll, topNews , findById, searchByTitle }
