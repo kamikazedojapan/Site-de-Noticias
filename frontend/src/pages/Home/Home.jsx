28b5be61090f0bb0e3872da2257b873c0ca0d2a8
kamikazedojapan <vi.nimarcio@hormail.com>
2025-02-22 14:41:06 -0300

    hook usestate

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,23 +1,33 @@
 import { Card } from "../../components/Cards/Card.jsx";
-import { Navbar } from "../../components/Navbar/Navbar.jsx"
-import { news } from "../../Datas.js"
+import { Navbar } from "../../components/Navbar/Navbar.jsx";
+// import { news } from "../../Datas.js"
 import { getAllPosts } from "../../services/postsServices.js";
 import { HomeBody } from "./HomeStyled.jsx";
- 
+import { useState } from "react";
+
 export default function Home() {
-    async function findAllPosts() {
-        const response = await getAllPosts();
-        console.log(response.data.results);
-    }
-    findAllPosts()
-    return(
-        <>
-        <Navbar/>
-        <HomeBody>
-            {news.map((item, index) => (
-                <Card key={index} news={item} />
-            ))}
-        </HomeBody>
-        </>
-    );
+  const [news, setNews] = useState([]);
+
+  async function findAllPosts() {
+    const response = await getAllPosts();
+    setNews(response.data.results);
+  }
+  findAllPosts();
+  return (
+    <>
+      <Navbar />
+      <HomeBody>
+        {news.map((item) => (
+          <Card 
+            key={item.id} 
+            title={item.title}
+            text={item.text}
+            banner={item.banner}
+            likes={item.likes.length}
+            comments={item.comments.length}
+          />
+        ))}
+      </HomeBody>
+    </>
+  );
 }
